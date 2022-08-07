47116f31b6eb9920f26fcf37620975dd42d699da
Huntrox <abde.had@hotmail.com>
2022-08-07 23:03:53 +0100

    Update BuildingManager.cs

--- Assets/Scripts/Runtime/Core/Building/BuildingManager.cs
+++ Assets/Scripts/Runtime/Core/Building/BuildingManager.cs
@@ -4,6 +4,15 @@ namespace HuntroxGames
 {
     public class BuildingManager : MonoBehaviour
     {
+        [SerializeField] private BuildingInfo[] buildingInfos;
+
         
+        public BuildingInfo[] BuildingInfos => buildingInfos;
+        public void OnBuildingClick(int index)
+        {
+            Debug.Log(buildingInfos[index].buildingName);
+        }
+        [ContextMenu("Test")]
+        public void TestBuild()=> OnBuildingClick(0);
     }
 }
