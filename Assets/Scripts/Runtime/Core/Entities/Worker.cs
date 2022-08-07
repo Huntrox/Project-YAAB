9ad4968da8fe9510dc152b0b148dcc789a7f5729
Huntrox <abde.had@hotmail.com>
2022-08-07 23:04:06 +0100

    Update Worker.cs

--- Assets/Scripts/Runtime/Core/Entities/Worker.cs
+++ Assets/Scripts/Runtime/Core/Entities/Worker.cs
@@ -1,7 +1,26 @@
-﻿namespace HuntroxGames
+﻿using HuntroxGames.Utils;
+using UnityEngine;
+
+namespace HuntroxGames
 {
     public class Worker : Entity
     {
+        [SerializeField,ReadOnly] private WorkerJob currentJob = WorkerJob.FreeLabor;
+        public WorkerJob CurrentJob
+        {
+            get => currentJob;
+            set => currentJob = value;
+        }
         
+    }
+
+    public enum WorkerJob
+    {
+        FreeLabor,
+        Lumberjack,
+        Miner,
+        Farmer,
+        Scouter,
+        Trainer,
     }
 }
