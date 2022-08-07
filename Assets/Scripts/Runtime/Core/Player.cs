105b131e28c30700d1319568a9d3f2eb4e118a2d
Huntrox <abde.had@hotmail.com>
2022-08-07 23:04:12 +0100

    Update Player.cs

--- Assets/Scripts/Runtime/Core/Player.cs
+++ Assets/Scripts/Runtime/Core/Player.cs
@@ -8,34 +8,42 @@ namespace HuntroxGames
     public class Player : NetworkBehaviour
     {
 
-        private readonly Dictionary<ResourceType, int> _resources = new Dictionary<ResourceType, int>
-        {
-            {ResourceType.Food, 200},
-            {ResourceType.Wood, 200},
-            {ResourceType.Metal, 200}
+        [SerializeField] private int workersCapacity = 3;
+        [SerializeField] private int herosCapacity = 3;
+
+        private readonly Dictionary<ResourceType, int> resources = new Dictionary<ResourceType, int>
+        { { ResourceType.Food, 200 },
+            { ResourceType.Wood, 200 },
+            { ResourceType.Metal, 200 }
         };
-        
-        
-        public event Action<ResourceType, int,bool> OnResourceChanged;
-        
-        
-        
-        
-        
-        
-        
-        
-        public bool UpdateResource(ResourceType type, int amount)
+
+
+        public int WorkersCapacity
         {
-            if (!_resources.ContainsKey(type) || _resources[type] + amount < 0)
+            get => workersCapacity;
+            set => workersCapacity = value;
+        }
+        public int HerosCapacity
+        {
+            get => herosCapacity;
+            set => herosCapacity = value;
+        }
+        
+        public event Action<ResourceType, int, bool> OnResourceChanged;
+
+        public bool HasEnoughResources (ResourceType type, int amount) 
+            => resources[type] >= amount;
+
+        public bool UpdateResource (ResourceType type, int amount)
+        {
+            if (!resources.ContainsKey (type) || resources[type] + amount < 0)
                 return false;
-            
+
             var negativeValue = amount < 0;
-            _resources[type] += amount;
-            OnResourceChanged?.Invoke(type, amount,negativeValue);
+            resources[type] += amount;
+            OnResourceChanged?.Invoke (type, amount, negativeValue);
             return true;
         }
-
 
     }
 }
