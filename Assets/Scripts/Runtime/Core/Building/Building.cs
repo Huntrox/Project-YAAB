ed459a4311cf9d453400af8c3f39f6a841488dbc
Huntrox <abde.had@hotmail.com>
2022-08-07 23:03:44 +0100

    Update Building.cs

--- Assets/Scripts/Runtime/Core/Building/Building.cs
+++ Assets/Scripts/Runtime/Core/Building/Building.cs
@@ -10,51 +10,60 @@ namespace HuntroxGames
     {
 
         [SerializeField, ReadOnly] protected string id;
-        [SerializeField] protected int maxWorkers = 1; 
+        [SerializeField] protected int maxWorkers = 1;
         [SerializeField] protected float buildTime = 2f;
-        [SerializeField, ReadOnly] protected List<Worker> currentWorkers = new List<Worker>();
+        [SerializeField, ReadOnly] protected List<Worker> currentWorkers = new List<Worker> ();
+        [SerializeField] protected WorkerJob buildingJob;
 
         protected bool isActive = false;
 
-        
-        
         public bool IsActive => isActive;
         public string ID
         {
             get => id;
             set => id = value;
         }
-        
+
         public event UnityAction OnBuildingStarted;
         public event UnityAction OnBuildingComplete;
 
-        public virtual bool AssignWorker(Worker worker)
+        public virtual bool AssignWorker (Worker worker)
         {
-            if (currentWorkers.Count >= maxWorkers || currentWorkers.Contains(worker)) return false;
-            currentWorkers.Add(worker);
+            if (currentWorkers.Count >= maxWorkers || currentWorkers.Contains (worker)) return false;
+            currentWorkers.Add (worker);
             return true;
         }
-        public virtual bool RemoveWorker(Worker worker)
+        public virtual bool RemoveWorker (Worker worker)
         {
-            if (!currentWorkers.Contains(worker)) return false;
-            currentWorkers.Remove(worker);
+            if (!currentWorkers.Contains (worker)) return false;
+            currentWorkers.Remove (worker);
             return true;
         }
 
-        public virtual bool Construct()
+        public virtual void StartPreview ()
         {
-            Debug.Log("Building Constructed");
+
+        }
+        public virtual bool Construct ()
+        {
+            Debug.Log ("Building Constructed");
             return true;
         }
-        
-        public virtual void Destroy()
+
+        public virtual void Destroy ()
         {
-            Debug.Log("Building Destroyed");
+            Debug.Log ("Building Destroyed");
         }
 
-        public virtual void Update()
+        public virtual void Update ()
         {
-            
+
+        }
+
+        private void OnValidate ()
+        {
+            if (id.IsNullOrEmpty ())
+                id = Utils.Utils.GenGuid ();
         }
     }
     public enum ResourceType
